eddd3dabd6672b08f365fee8da8b6cbac4423215
Terauchi Taku <c0b2111568@edu.teu.ac.jp>
2022-09-27 16:45:44 +0900

    alphabetQuiz

---
+++ ex01/alphabet.py
@@ -0,0 +1,60 @@
+import random
+import datetime
+import datetime
+
+num_of_alphabet = 26  
+num_of_all_chars = 10 
+num_of_abs_chars = random.randint(1,10)
+num_of_trials = 1000
+
+def shutudai(alphabet):
+    all_chars = random.sample(alphabet, num_of_all_chars)
+    print("対象文字：", end="")
+    for c in sorted(all_chars): 
+        print(c, end=" ")
+    print()
+
+    
+    abs_chars = random.sample(all_chars, num_of_abs_chars)
+    print("表示文字：", end="")
+    for c in all_chars: 
+        if c not in abs_chars: 
+            print(c, end=" ")
+    print()
+    print("デバッグ用欠損文字：", abs_chars)
+    return abs_chars
+
+
+def kaito(seikai):
+    num = int(input("欠損文字はいくつあるでしょうか？："))
+    if num != num_of_abs_chars:
+        print("不正解です．")
+    else:
+        print("正解です．では，具体的に欠損文字を1つずつ入力してください．")
+        for i in range(num):
+            c = input(f"{i+1}文字目を入力してください：")
+            if c not in seikai:
+                print("不正解です．またチャレンジしてください．")
+                return False
+            else:
+                seikai.remove(c) 
+        else:
+            print("欠損文字も含めて完全正解です！！！")
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    alphabet = [chr(i+65) for i in range(num_of_alphabet)]
+    
+    for _ in range(num_of_trials):
+        st = datetime.datetime.now()
+        abs_chars = shutudai(alphabet)  
+        ret = kaito(abs_chars)
+        if ret:
+            ed =datetime.datetime.now()
+            print(f"回答時間：{(ed-st).seconds}秒")
+            break
+        else:
+            print("-"*20)
+    
