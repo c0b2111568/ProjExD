f83479fc537f7a285416303009ba0757b0c13567
Terauchi Taku <c0b2111568@edu.teu.ac.jp>
2022-10-04 16:33:30 +0900

    様々な計算方法をできるようにした

--- ex02/calc.py
+++ ex02/calc.py
@@ -15,23 +15,35 @@ def click_equal(event):
 
 
 root = tk.Tk() 
-root.geometry("300x500")
+root.geometry("400x700")
 
 entry = tk.Entry(root, width=10, font=(", 40"), justify="right") 
 entry.grid(row=0, column=0, columnspan=3)
 
-r, c = 1, 0 
+r_num, c_num = 1, 0 
 numbers = list(range(9, -1, -1)) 
-pulus = ["+"] 
-for i, num in enumerate(numbers+pulus, 1):
-    botton = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
-    botton.bind("<1>", click_number)
-    botton.grid(row=r, column=c)
-    c += 1
+Calcula = ["+","-","*","/","."] 
+
+for i, num in enumerate(numbers, 1):
+    botton_num = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2,bg = "#FFFFFF")
+    botton_num.bind("<1>", click_number)
+    botton_num.grid(row=r_num, column=c_num)
+    c_num += 1
     if i%3 == 0:
-        r += 1
-        c = 0
-botton = tk.Button(root, text = f"=",font=("",30),width=4,height=2)
+        r_num += 1
+        c_num = 0
+
+r_Ca, c_Ca = 1, 0 
+for j in Calcula:
+    botton_Ca = tk.Button(root, text=f"{j}", font=("", 30), width=4, height=2,bg="#FFD700")
+    botton_Ca.bind("<1>", click_number)
+    botton_Ca.grid(row=r_Ca, column=3-c_Ca)
+    r_Ca += 1
+    if r_Ca==5:
+        r_Ca=4
+        c_Ca=2
+
+botton = tk.Button(root, text = f"=",font=("",30),width=4,height=2,bg="#FFD700")
 botton.bind("<1>", click_equal)
-botton.grid(row=r, column=c)
+botton.grid(row=r_num, column=c_num+1)
 root.mainloop()
