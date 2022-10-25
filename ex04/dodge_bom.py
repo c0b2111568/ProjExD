00eb38288096c778a76e9af1c43b77c47d0e528c
Terauchi Taku <c0b2111568@edu.teu.ac.jp>
2022-10-25 17:30:31 +0900

    追加機能

--- ex04/dodge_bom.py
+++ ex04/dodge_bom.py
@@ -1,10 +1,39 @@
+from inspect import BoundArguments
 import pygame as pg
 import sys
+from random import randint
+import pygame
+import time
+
+def alarm():
+    pygame.mixer.init(frequency = 44100)    # 初期設定
+    pygame.mixer.music.load("MP3/bomb.mp3")     # 音楽ファイルの読み込み
+    pygame.mixer.music.play(1)              # 音楽の再生回数(1回)
+    time.sleep(1)
+    pygame.mixer.music.stop()               # 再生の終了
+    return 0
+
+def check_bound(obj_rct, scr_rct):
+    """
+    obj_rct：こうかとんrct，または，爆弾rct
+    scr_rct：スクリーンrct
+    領域内：+1／領域外：-1
+    """
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: 
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: 
+        tate = -1
+    return yoko, tate
+
+    # 練習6
+
 
 def main():
     # 練習1
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_rct = scrn_sfc.get_rect()
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
 
@@ -14,29 +43,70 @@ def main():
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
 
+    # 練習5
+    bomb_sfc = pg.Surface((20, 20)) # 空のSurface
+    bomb_sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を透過させる
+    pg.draw.circle(bomb_sfc, (randint(0,255), randint(0,255), randint(0,255)), (10, 10), 10) # 円を描く
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = randint(0, scrn_rct.width)
+    bomb_rct.centery = randint(0, scrn_rct.height)
+    # 練習6
+    vx, vy = +1, +1
+
 
     clock = pg.time.Clock() # 練習1
+    pygame.mixer.init(frequency = 44100)    # 初期設定
+    pygame.mixer.music.load("MP3/BGM.mp3")     # 音楽ファイルの読み込み
+    pygame.mixer.music.play(1) 
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct) # 練習2
+
         
         for event in pg.event.get(): # 練習2
             if event.type == pg.QUIT:
+                pygame.mixer.music.stop()
                 return
-        key_states=pg.key.get_pressed()
-        if key_states[pg.K_UP]: tori_rct.centery -= 1
-        if key_states[pg.K_DOWN]:tori_rct.centery += 1
-        if key_states[pg.K_LEFT]:tori_rct.centerx -= 1
-        if key_states[pg.K_RIGHT]:tori_rct.centerx += 1
-        
 
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_UP]:    tori_rct.centery -= 1
+        if key_states[pg.K_DOWN]:  tori_rct.centery += 1
+        if key_states[pg.K_LEFT]:  tori_rct.centerx -= 1
+        if key_states[pg.K_RIGHT]: tori_rct.centerx += 1
+        yoko, tate = check_bound(tori_rct, scrn_rct)
+        if yoko == -1:
+            if key_states[pg.K_LEFT]: 
+                tori_rct.centerx += 1
+            if key_states[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
+        if tate == -1:
+            if key_states[pg.K_UP]: 
+                tori_rct.centery += 1
+            if key_states[pg.K_DOWN]:
+                tori_rct.centery -= 1            
         scrn_sfc.blit(tori_sfc, tori_rct) # 練習3
+
+        # 連取7
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
+        bomb_rct.move_ip(vx, vy) # 練習6
+        if yoko == -1 or tate ==-1: 
+            vx *=1.2
+            vy *=1.2
+
+            
+        
+    # 練習6
+       
+        scrn_sfc.blit(bomb_sfc, bomb_rct) # 練習5
+
+        # 練習8
+        if tori_rct.colliderect(bomb_rct): # こうかとんrctが爆弾rctと重なったら
+            alarm()
+            return
 
         pg.display.update() #練習2
         clock.tick(1000)
-
-
-   
-
 
 if __name__ == "__main__":
     pg.init() # 初期化
